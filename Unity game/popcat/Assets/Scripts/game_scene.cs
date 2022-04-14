b648d694b642d5b26ea38ed70355e50bb1ecb60c
wai713952 <wai.test@mail.kmutt.ac.th>
2022-04-14 18:51:11 +0700

    initial upload
    - web concept doc
    - Unity games (Basketball, Rock Paper Scissors, Popcat) with no web data collection

---
+++ Unity game/popcat/Assets/Scripts/game_scene.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class game_scene : MonoBehaviour
+{
+    public float score;
+    public bool canPop;
+
+    public Image popImage;
+    public Sprite normalPic;
+    public Sprite popPic;
+
+    public TMP_Text scoreText;
+
+    public GameObject blackPanel;
+    public GameObject startButton;
+    public GameObject stopButton;
+
+    void Start()
+    {
+        popImage.GetComponent<Image>();
+        scoreText.GetComponent<TMP_Text>();
+        blackPanel.SetActive(true);
+        startButton.SetActive(true);
+        stopButton.SetActive(false);
+        canPop = true;
+    }
+
+    public void PopButton()
+    {
+        if (canPop)
+        {
+            score++;
+            scoreText.text = "Congratulation: " + score.ToString();
+            canPop = false;
+            popImage.sprite = popPic;
+            Invoke("WaitToPop", 0.05f);
+        }
+    }
+    private void WaitToPop()
+    {
+        canPop = true;
+        popImage.sprite = normalPic;
+    }
+
+    public void StartButton()
+    {
+        blackPanel.SetActive(false);
+        startButton.SetActive(false);
+        stopButton.SetActive(true);
+    }
+
+    public void StopButton()
+    {
+        blackPanel.SetActive(true);
+        startButton.SetActive(true);
+        stopButton.SetActive(false);
+
+        //send var to web
+
+        score = 0;
+        scoreText.text = "Congratulation: 0";
+    }
+}
---
+++ Unity game/rock paper scissor/Assets/script/game_scene_controller.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class game_scene_controller : MonoBehaviour
+{
+    string[] answerChoice = new string[] { "Rock", "Paper", "Scissors" };
+    string question;
+    string answer;
+
+    public TMP_Text questionText;
+    public TMP_Text answerText;
+    public TMP_Text scoreText;
+    public TMP_Text hpText;
+    public GameObject loseText;
+    public GameObject UIBlocker;
+    public GameObject rockBut;
+    public GameObject paperBut;
+    public GameObject scissorsBut;
+
+    public float HP;
+    public float score;
+
+    public void Start()
+    {
+        questionText.GetComponent<TMP_Text>();
+        answerText.GetComponent<TMP_Text>();
+        scoreText.GetComponent<TMP_Text>();
+        hpText.GetComponent<TMP_Text>();
+        questionText.text = "";
+        answerText.text = "";
+        scoreText.text = "score = 0";
+        hpText.text = "HP: " + HP.ToString();
+        UIBlocker.SetActive(true);
+        loseText.SetActive(false);
+        rockBut.SetActive(false);
+        paperBut.SetActive(false);
+        scissorsBut.SetActive(false);
+    }
+
+    public void RandomQuestion()
+    {
+        question = answerChoice[Random.Range(0,3)];
+        print(question);
+    }
+
+    public void AnswerButton()
+    {
+        answer = EventSystem.current.currentSelectedGameObject.name;
+
+        if (answer == question)
+        {
+            print("right");
+            if (HP + 10 > 100)
+            {
+                HP = 100;
+            }
+            else
+            {
+                HP += 10;
+            }
+            score++;
+            scoreText.text = "score " + score.ToString();
+        }
+        else
+        {
+            print("wrong");
+            HP -= 10;
+            if (HP <= 0)
+            {
+                questionText.text = "";
+                answerText.text = "";
+                hpText.text = "HP: " + HP.ToString();
+                UIBlocker.SetActive(true);
+                loseText.SetActive(true);
+                Invoke("QuitButton", 3f);
+                return;
+            }
+        }
+
+        answerText.text = answer;
+        questionText.text = question;
+        hpText.text = "HP: " + HP.ToString();
+        UIBlocker.SetActive(true);
+        Invoke("ShowQuestionAnswer", 0.5f);
+    }
+    public void ShowQuestionAnswer()
+    {
+        answerText.text = "";
+        questionText.text = "";
+        RandomQuestion();
+        UIBlocker.SetActive(false);
+    }
+
+    public void StartButton()
+    {
+        Destroy(EventSystem.current.currentSelectedGameObject.gameObject);
+        UIBlocker.SetActive(false);
+        rockBut.SetActive(true);
+        paperBut.SetActive(true);
+        scissorsBut.SetActive(true);
+        RandomQuestion();
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
